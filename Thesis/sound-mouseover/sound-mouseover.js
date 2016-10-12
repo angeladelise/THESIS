fcc0cb914c14a9adc3066afdb9957867ced7d330
angeladelise <delia914@newschool.edu>
2016-10-12 19:43:49 -0400

    update

---
+++ Thesis/sound-mouseover/sound-mouseover.js
@@ -0,0 +1,326 @@
+
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "welcome.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("welcome");
+audio.play();
+}
+}
+}
+
+
+function playclipMen() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "men.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("men");
+audio.play();
+}
+}
+}
+
+
+function playclipWomen() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "women.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("women");
+audio.play();
+}
+}
+}
+
+
+
+function playclipHome() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "home.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("home");
+audio.play();
+}
+}
+}
+
+function playclipShirts() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "shirts.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("shirts");
+audio.play();
+}
+}
+}
+
+function playclipPants() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "pants.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("pants");
+audio.play();
+}
+}
+}
+
+function playclipJackets() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "jackets.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("jackets");
+audio.play();
+}
+}
+}
+
+function playclipShoes() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "shoes.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("shoes");
+audio.play();
+}
+}
+}
+
+function playclipShoppingCartEmpty() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "shopping-cart-empty.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("shopping-cart-empty");
+audio.play();
+}
+}
+}
+
+function playclipPrice18() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "price-18.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("price");
+audio.play();
+}
+}
+}
+
+function playclipSizesSML() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "sizes-SML.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sizes");
+audio.play();
+}
+}
+}
+
+function playclipShirtColors() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "shirt-colors.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("shirt-colors");
+audio.play();
+}
+}
+}
+
+function playclipDescription() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "description.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("description");
+audio.play();
+}
+}
+}
+
+function playclipWash() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "wash-instructions.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("wash");
+audio.play();
+}
+}
+}
+
+function playclipAddtoCart() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "add-shopping-cart.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("addshoppingcart");
+audio.play();
+}
+}
+}
+
+function playclipAddedtoCart() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "added-shopping-cart.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("addedshoppingcart");
+audio.play();
+}
+}
+}
+
+
+
+function playclipButtonDown() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "buttondown.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("buttondown");
+audio.play();
+}
+}
+}
+
+function playclipGraphicTShirt() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "graphictshirt.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("graphictshirt");
+audio.play();
+}
+}
+}
+
+function playclipKnitTop() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "knittop.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("knittop");
+audio.play();
+}
+}
+}
+
+function playclipShortSleeveButton() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "shortsleevebutton.ogg";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("shortsleevebutton");
+audio.play();
+}
+}
+}
